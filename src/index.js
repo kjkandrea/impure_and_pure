ce07ea6d0669363ade3d86c940433eb4ed689afc
andrea <kjkandrea@gmail.com>
2021-10-10 15:18:49 +0900

    Add button prototype

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { pipe } = window.R
 const app = {
   memberListEl: document.getElementById('member-list'),
   init () {
-    app.render().error(console.error)
+    app.render().catch(console.error)
   },
   async render () { // render main 함수. impure + pure 합성
     const members = await app.fetchMembers()
@@ -24,17 +24,27 @@ const app = {
   },
   getRowEl (object) { // pure
     const tr = document.createElement('tr')
+
     Object
       .values(object)
       .map(data => app.getColEl(data))
       .forEach(col => tr.append(col))
+
+    const buttonCol = tr.append(app.getColEl(app.getButton('details')))
     return tr
   },
   getColEl (data) { // pure
+    const isEl = typeof data === 'object' && data instanceof HTMLElement
+
     const td = document.createElement('td')
-    td.innerText = data
+    isEl ? td.append(data) : td.innerText = data
     return td
   },
+  getButton (text) {
+    const button = document.createElement('button')
+    button.innerText = text
+    return button
+  }
 }
 
 document.addEventListener('DOMContentLoaded', app.init.bind(undefined))
