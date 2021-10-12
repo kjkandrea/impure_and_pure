d9bad6e8fa390faa626d79bb50d4b6693698fccc
andrea <kjkandrea@gmail.com>
2021-10-12 11:36:51 +0900

    Add sample code
    발표용 샘플

--- src/presentation.js
+++ src/presentation.js
@@ -3,7 +3,7 @@ import { pipe, curry } from 'ramda'
 /**
  * form Data 를 post 하기
  * 1. 유효성 검증
- * 2. post 요청 양식에 맞게 가공
+ * 2. post 요청 양식에 맞게 request 가공
  * 3. post 요청
  * 4. post 이행된 후 next ( 다음에 무엇을 할지 )
  */
